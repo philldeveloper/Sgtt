8e5b14f664c99f69eee60416550800da79143e44
Tauan Almeida <tauan_almeida@hotmail.com>
2019-09-06 12:39:45 -0300

    algumas classes css retiradas

--- resources/views/researcher/faq/index.blade.php
+++ resources/views/researcher/faq/index.blade.php
@@ -2,40 +2,39 @@
 
 @section('content')
 
-<style>#wrapper #content-wrapper {background-color: white !important;}
+<style>#wrapper #content-wrapper {background-color: #f5f5f5 !important;}
 .faq-container{
   display: flex;
-  flex-direction: column;
   align-items: center;
   justify-content: center;
+  height: 72px;
 }
 
-.col > .card-res-hover{
-  width: 300px;
-  min-width: 250px;
+.box-shadow{
   -webkit-box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.04);
-    -moz-box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.04);
-    box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.04);
+  -moz-box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.04);
+  box-shadow: 0 0 5px 5px rgba(0, 0, 0, 0.04);
 }
 
-
+.col > .card{
+  
+}
 
 </style>
 
 <!-- Begin Page Content -->
-<div class="container-fluid bg-white pt-5">
+<div class="mb-4 col-md-12 faq-container bg-white box-shadow">
+  <h1 class="h3 mb-0 text-gray-800 font-weight-bold">Como podemos ajudar você?</h1>
+</div>
+<div class="container-fluid pt-5">
 
   <!-- Page Heading -->
-  <div class="mb-4 col-md-12 faq-container">
-    <h1 class="h3 mb-0 text-gray-800 font-weight-bold">Como podemos ajudar você?</h1>
-    <p class="lead mt-3">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magni dicta alias enim quasi sapiente saepe, repellat soluta commodi delectus doloribus, veniam odit hic ratione quae modi earum dolor dignissimos quas?</p>
-  </div>
 
   <!-- Content Row -->
   <div class="row justify-content-center">
     <!-- Earnings (Monthly) Card Example -->
     <div class="col col-sm-12 col-md-4 mt-4 mb-4">
-      <div class="card card-res-hover h-100 py-2 border border-success">
+      <div class="card py-2 box-shadow">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -51,7 +50,7 @@
 
     <!-- Earnings (Monthly) Card Example -->
     <div class="col col-sm-12 col-md-4 mt-4 mb-4">
-      <div class="card card-res-hover h-100 py-2 border border-success">
+      <div class="card py-2 box-shadow">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -68,7 +67,7 @@
 <div class="row justify-content-center">
   <!-- Earnings (Monthly) Card Example -->
   <div class="col col-sm-12 col-md-4 mt-4 mb-4">
-      <div class="card card-res-hover h-100 py-2 border border-success">
+      <div class="card py-2 box-shadow">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
@@ -88,7 +87,7 @@
 
     <!-- Earnings (Monthly) Card Example -->
     <div class="col col-sm-12 col-md-4 mt-4 mb-4">
-      <div class="card card-res-hover h-100 py-2 border border-success">
+      <div class="card py-2 box-shadow">
         <div class="card-body">
           <div class="row no-gutters align-items-center">
             <div class="col mr-2">
