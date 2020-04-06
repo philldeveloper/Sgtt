749fd7de74a8421525d68a8ecf7720c96cd20138
philldeveloper <philldeveloper@gmail.com>
2020-04-06 15:10:33 -0300

    0604 15:10

--- resources/views/contratos/tt/repasse/show.blade.php
+++ resources/views/contratos/tt/repasse/show.blade.php
@@ -1335,31 +1335,90 @@
                   <h4 class="text-uppercase font-weight-bold roboto-font text-left text-black mb-3 p-2">19. CLÁUSULA DÉCIMA NONA - DO FORO <a href="#"><i class="fas fa-fw fa-question-circle ml-2"></i></a></h4><hr><br>
       
                   <p><b>19.1.</b>Fica eleito o foro da Justiça Federal, Seção Judiciária do Estado de <input class="myform-control text-black"  type="text" name="estado_foro" id="estado_foro" placeholder="{{$contratos_cr->estado_foro}}" disabled="">, cidade de <input  class="myform-control" type="text" name="cidade_foro" id="cidade_foro" placeholder="{{$contratos_cr->cidade_foro}}" disabled="">,
-                                para dirimir quaisquer litígios oriundos deste ACORDO, nos termos do inciso I do artigo 109
-                                da Constituição Federal.
-                                E como prova de assim haverem livremente pactuado, firmam os PARCEIROS o presente
-                                instrumento em 3 (três) vias, de igual teor e forma, para que produza entre si os efeitos legais.
-                                </p>
-                      <input type="text" class="myform-control text-black"  name="cidade_uf_dia_foro" id="cidade_uf_dia_foro" placeholder="{{$contratos_cr->cidade_uf_dia_foro}}" disabled="">   <br>   
-                    <div class="mb-4 row justify-content-center">
-                      <div class="col-6 align-self-center">
-                        <div class="input-control-center">
-                          <p class="mt-4">Pelo(a)  <span class="text-blue">ICT:</span> <input class="myform-control text-black" type="text" name="ict_inst_foro" id="ict_inst_foro" placeholder="{{$contratos_cr->ict_inst_foro}}" disabled=""> </p>   
-                            <input class="mb-2 myform-control text-black" type="text" name="nome_rep_foro" id="nome_rep_foro" placeholder="{{$contratos_cr->nome_rep_foro}}" disabled=""> 
-                            <br><input class="mb-2  myform-control text-black" type="text" name="cargo_rep_foro" id="cargo_rep_foro" placeholder="{{$contratos_cr->cargo_rep_foro}}" disabled=""> 
-                          </div>
-                      </div>
+                  para dirimir quaisquer litígios oriundos deste ACORDO, nos termos do inciso I do artigo 109
+                  da Constituição Federal.
+                  E como prova de assim haverem livremente pactuado, firmam os PARCEIROS o presente
+                  instrumento em 3 (três) vias, de igual teor e forma, para que produza entre si os efeitos legais.
+                  </p>
+
+                  <div class="form-row mt-3">
+                    <div class="form-group col-md-3">
+                      <label for="cidade_uf_dia_foro" class="font-weight-bold text-black">Cidade, UF, Dia do Mês e Ano</label>
+                      <input type="text" 
+                        class="form-control text-black font-weight-bold" 
+                        name="cidade_uf_dia_foro"
+                        id="cidade_uf_dia_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->cidade_uf_dia_foro}}">
                     </div>
-                       <div class="mt-4 row justify-content-center">
-                         <div class="col-6 align-self-center">
-                           <div class="input-control-center">
-                             <p>Pelo(a) <span class="text-blue">PARCEIRO PRIVADO:</span> <input class="myform-control text-black" type="text" name="nome_inst_privado_foro" id="nome_inst_privado_foro" placeholder="{{$contratos_cr->nome_inst_privado_foro}}" disabled=""> </p>      
-                             <input class="mb-2 myform-control text-black" type="text" name="nome_rep_privado_foro" id="nome_rep_privado_foro" placeholder="{{$contratos_cr->nome_rep_privado_foro}}" disabled=""> 
-                             <br><input class="mb-2  myform-control text-black" type="text" name="cargo_rep_privado_foro" id="cargo_rep_privado_foro" placeholder="{{$contratos_cr->cargo_rep_privado_foro}}" disabled=""> 
-                           </div>
-                         </div>
-                       </div>
-                </div>
+                  </div><!-- /.row-->
+
+                  <div class="form-row mt-3">
+                    <div class="form-group col-md-3">
+                      <label for="ict_inst_foro" class="font-weight-bold text-black">ICT:</label>
+                      <input type="text" 
+                        class="form-control text-black font-weight-bold"
+                        name="ict_inst_foro" 
+                        id="ict_inst_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->ict_inst_foro}}">
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="nome_rep_foro" class="font-weight-bold text-black">Representante:</label>
+                      <input type="text" 
+                        class="form-control text-black font-weight-bold" 
+                        name="nome_rep_foro" id="nome_rep_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->nome_rep_foro}}">
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="cargo_rep_foro" class="font-weight-bold text-black">Cargo</label>
+                      <input type="email" 
+                        class="form-control text-black font-weight-bold"
+                        name="cargo_rep_foro" 
+                        id="cargo_rep_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->cargo_rep_foro}}">
+                    </div>
+                  </div><!-- /.row-->
+
+                  <div class="form-row mt-3">
+                    <div class="form-group col-md-3">
+                      <label for="nome_inst_privado_foro" class="font-weight-bold text-black">Parceiro Privado:</label>
+                      <input type="text" 
+                        class="form-control text-black font-weight-bold"
+                        name="nome_inst_privado_foro" 
+                        id="nome_inst_privado_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->nome_inst_privado_foro}}">
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="nome_rep_privado_foro" class="font-weight-bold text-black">Representante:</label>
+                      <input type="text" 
+                        class="form-control text-black font-weight-bold" 
+                        name="nome_rep_privado_foro" id="nome_rep_privado_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->nome_rep_privado_foro}}">
+                    </div>
+                    <div class="form-group col-md-3">
+                      <label for="cargo_rep_privado_foro" class="font-weight-bold text-black">Cargo</label>
+                      <input type="email" 
+                        class="form-control text-black font-weight-bold"
+                        name="cargo_rep_privado_foro" 
+                        id="cargo_rep_privado_foro" 
+                        disabled 
+                        readonly="" 
+                        placeholder="{{$contratos_cr->cargo_rep_privado_foro}}">
+                    </div>
+                  </div><!-- /.row-->
+
+                </div><!--text-justify-->
               </div>
           </div>
         <div class="tab-pane " id="clausulaF" role="tabpanel" aria-labelledby="clausulaF-tab">
