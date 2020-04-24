c795f3e3b7938f0f06a876a2bcc37f4d61064640
philldeveloper <philldeveloper@gmail.com>
2020-04-24 11:40:05 -0300

    2404

--- resources/views/pesquisador/faq/index.blade.php
+++ resources/views/pesquisador/faq/index.blade.php
@@ -22,69 +22,63 @@
   <h1 class="h3 mb-0 text-gray-800 font-weight-bold">Como podemos ajudar você?</h1>
 </div>
 
+<div class="container p-5">
+  <div class="row justify-content-center">
+    <ul class="nav nav-pills mb-3" id="pills-tab" role="tablist">
+      <li class="nav-item">
+        <a class="nav-link py-3 mb-4 font-weight-bold active" id="pills-ask-tab" data-toggle="pill" href="#pills-ask" role="tab" aria-controls="pills-ask" aria-selected="true">Perguntas Frequentes</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link py-3 mb-4 font-weight-bold" id="pills-tutorials-tab" data-toggle="pill" href="#pills-tutorials" role="tab" aria-controls="pills-tutorials" aria-selected="false">Tutoriais</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link py-3 mb-4 font-weight-bold" id="pills-docs-tab" data-toggle="pill" href="#pills-docs" role="tab" aria-controls="pills-docs" aria-selected="false">Documentação</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link py-3 mb-4 font-weight-bold" id="pills-nit-tab" data-toggle="pill" href="#pills-nit" role="tab" aria-controls="pills-nit" aria-selected="false">Sobre o NIT</a>
+      </li>
+      <li class="nav-item">
+        <a class="nav-link py-3 mb-4 font-weight-bold" id="pills-marca-tab" data-toggle="pill" href="#pills-marca" role="tab" aria-controls="pills-marca" aria-selected="false">Manual da Marca</a>
+      </li>
+    </ul>
+  </div>
+  <div class="tab-content shadow border border-primary bg-white p-5" id="pills-tabContent">
 
-<div class="col-12">
-  <div class="card m-3 p-5">
-    <div class="bd-example bd-example-tabs">
-  <div class="row">
-    <div class="col-3">
-      <div class="nav flex-column nav-pills" id="v-pills-tab" role="tablist" aria-orientation="vertical">
-        <a class="nav-link font-weight-bold p-4 active" id="v-pills-home-tab" data-toggle="pill" href="#v-pills-home" role="tab" aria-controls="v-pills-home" aria-selected="true">Perguntas Frequentes</a>
-        <a class="nav-link font-weight-bold p-4" id="v-pills-profile-tab" data-toggle="pill" href="#v-pills-profile" role="tab" aria-controls="v-pills-profile" aria-selected="false">Tutoriais</a>
-        <a class="nav-link font-weight-bold p-4" id="v-pills-messages-tab" data-toggle="pill" href="#v-pills-messages" role="tab" aria-controls="v-pills-messages" aria-selected="false">Documentação</a>
-        <a class="nav-link font-weight-bold p-4" id="v-pills-settings-tab" data-toggle="pill" href="#v-pills-settings" role="tab" aria-controls="v-pills-settings" aria-selected="false">Sobre o NIT</a>
-        <a class="nav-link font-weight-bold p-4" id="v-pills-manual-tab" data-toggle="pill" href="#v-pills-manual" role="tab" aria-controls="v-pills-manual" aria-selected="false">Manual da Marca</a>
-      </div>
+    <div class="tab-pane fade show active" id="pills-ask" role="tabpanel" aria-labelledby="pills-ask-tab">
+      <h2 class="font-weight-bold text-dark mb-5">Perguntas Frequentes</h2>
+      <h4 class="">Posso criar mais de um contrato ao mesmo tempo?</h4><hr>
+      <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
     </div>
-    <div class="col-9">
-      <div class="tab-content ml-5" id="v-pills-tabContent">
-        <div class="tab-pane fade show active" id="v-pills-home" role="tabpanel" aria-labelledby="v-pills-home-tab">
-          <h2 class="font-weight-bold text-dark bg-light mb-5">Perguntas Frequentes</h2>
 
-          <h4 class="font-weight-bold font-italic">Posso criar mais de um contrato ao mesmo tempo?</h4><hr>
-          <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          </p>
-          <br>
-          <h4 class="font-weight-bold font-italic">Posso criar mais de um contrato ao mesmo tempo?</h4><hr>
-          <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.</p>
-          <br>
-          <h4 class="font-weight-bold font-italic">Posso criar mais de um contrato ao mesmo tempo?</h4><hr>
-          <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          </p>
-          <br>
-          <h4 class="font-weight-bold font-italic">Posso criar mais de um contrato ao mesmo tempo?</h4><hr>
-          <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
-          </p>
-          <br>
-        </div>
-        <div class="tab-pane fade" id="v-pills-profile" role="tabpanel" aria-labelledby="v-pills-profile-tab">
-          <h2 class="font-weight-bold text-dark bg-light mb-5">Tutoriais</h2>
-          <p>Culpa dolor voluptate do laboris laboris irure reprehenderit id incididunt duis pariatur mollit aute magna pariatur consectetur. Eu veniam duis non ut dolor deserunt commodo et minim in quis laboris ipsum velit id veniam. Quis ut consectetur adipisicing officia excepteur non sit. Ut et elit aliquip labore Lorem enim eu. Ullamco mollit occaecat dolore ipsum id officia mollit qui esse anim eiusmod do sint minim consectetur qui.</p>
-          <br>
-          <iframe width="100%" height="500px" src="" class="bg-light"></iframe>
-        </div>
-        <div class="tab-pane fade" id="v-pills-messages" role="tabpanel" aria-labelledby="v-pills-messages-tab">
-          <h2 class="font-weight-bold text-dark bg-light mb-5">Documentação</h2>
-          <p>Fugiat id quis dolor culpa eiusmod anim velit excepteur proident dolor aute qui magna. Ad proident laboris ullamco esse anim Lorem Lorem veniam quis Lorem irure occaecat velit nostrud magna nulla. Velit et et proident Lorem do ea tempor officia dolor. Reprehenderit Lorem aliquip labore est magna commodo est ea veniam consectetur.</p>
-        </div>
-        <div class="tab-pane fade" id="v-pills-settings" role="tabpanel" aria-labelledby="v-pills-settings-tab">
-          <h2 class="font-weight-bold text-dark bg-light mb-5">Sobre o NIT</h2>
-          <p>Eu dolore ea ullamco dolore Lorem id cupidatat excepteur reprehenderit consectetur elit id dolor proident in cupidatat officia. Voluptate excepteur commodo labore nisi cillum duis aliqua do. Aliqua amet qui mollit consectetur nulla mollit velit aliqua veniam nisi id do Lorem deserunt amet. Culpa ullamco sit adipisicing labore officia magna elit nisi in aute tempor commodo eiusmod.</p>
-        </div>
-        <div class="tab-pane fade" id="v-pills-manual" role="tabpanel" aria-labelledby="v-pills-manual-tab">
-          <h2 class="font-weight-bold text-dark bg-light mb-5">Manual da Marca</h2>
-          <p>Eu dolore ea ullamco dolore Lorem id cupidatat excepteur reprehenderit consectetur elit id dolor proident in cupidatat officia. Voluptate excepteur commodo labore nisi cillum duis aliqua do. Aliqua amet qui mollit consectetur nulla mollit velit aliqua veniam nisi id do Lorem deserunt amet. Culpa ullamco sit adipisicing labore officia magna elit nisi in aute tempor commodo eiusmod.</p>
-        </div>
-      </div>
+    <div class="tab-pane fade" id="pills-tutorials" role="tabpanel" aria-labelledby="pills-tutorials-tab">
+      <h2 class="font-weight-bold text-dark mb-5">Tutoriais</h2>
+      <h4 class="">Como gerar um contrato</h4><hr>
+      <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+      <a href="#" class="btn btn-success btn-lg">Ver PDF</a>
+    </div>
+
+
+    <div class="tab-pane fade" id="pills-docs" role="tabpanel" aria-labelledby="pills-docs-tab">
+      <h2 class="font-weight-bold text-dark mb-5">Documentação</h2>
+      <h4 class="">Lorem Ipsum</h4><hr>
+      <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+      <a href="#" class="btn btn-primary btn-lg" target="blank">Abrir Link</a>
+    </div>
+
+    <div class="tab-pane fade" id="pills-nit" role="tabpanel" aria-labelledby="pills-nit-tab">
+      <h2 class="font-weight-bold text-dark mb-5">Sobre o NIT</h2>
+      <h4 class="">Lorem Ipsum</h4><hr>
+      <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+      <a href="#" class="btn btn-outline-primary btn-lg" target="blank">Abrir Link</a>
+    </div>
+
+    <div class="tab-pane fade" id="pills-marca" role="tabpanel" aria-labelledby="pills-marca-tab">
+      <h2 class="font-weight-bold text-dark mb-5">Manual da Marca</h2>
+      <h4 class="">Lorem Ipsum</h4><hr>
+      <p>Cillum ad ut irure tempor velit nostrud occaecat ullamco aliqua anim Lorem sint.Lorem ipsum dolor sit amet consectetur adipisicing elit, sed do eiusmodtempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo</p>
+      <a href="{{asset('assets/manual_sgtt.pdf')}}" class="btn btn-lg btn-success py-3 mt-3" target="blank">Baixar Manual</a>
     </div>
   </div>
-</div>
-  </div>
-</div>
-
-<div class="container m-5">
-
-
 </div>
 
 
